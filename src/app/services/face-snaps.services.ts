f2224e4ae167ec308bb1446a8f674f00f27947b3
FKC <batfkc@gmail.com>
2022-05-08 21:46:31 +0100

    Create a service for faceSnaps

---
+++ src/app/services/face-snaps.services.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { FaceSnap } from "../models/face-snap.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FaceSnapsService {
+    faceSnaps: FaceSnap[] = [
+        {
+          title: "Cortes",
+          description: "Mon mauvais ami",
+          imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+          createdDate: new Date(),
+          snaps: 50,
+          btnContent: "Oh snaps !",
+          userSnapped: false
+        },
+  
+        {
+          title: 'Three Rock Mountain',
+          description: 'Un endroit magnifique pour les randonnées.',
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+          createdDate: new Date(),
+          snaps: 165,
+          btnContent: "Oh snaps !",
+          userSnapped: false,
+          location: "Gotham"
+        },
+  
+        {
+          title: 'Un bon repas',
+          description: 'Mmmh que c\'est bon !',
+          imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+          createdDate: new Date(),
+          snaps: 200,
+          btnContent: "Oh snaps !",
+          userSnapped: false
+        }
+      ];
+}
