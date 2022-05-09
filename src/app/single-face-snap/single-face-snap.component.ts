cd07354d2ecbd95272c58c116d2370c867e1e137
FKC <batfkc@gmail.com>
2022-05-09 13:07:25 +0100

    Single snap view added with routing

--- src/app/single-face-snap/single-face-snap.component.ts
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -1,26 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.services';
 
 @Component({
-  selector: 'app-face-snap',
-  templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.scss']
+  selector: 'app-single-face-snap',
+  templateUrl: './single-face-snap.component.html',
+  styleUrls: ['./single-face-snap.component.scss']
 })
-export class FaceSnapComponent implements OnInit {
-
-  @Input() faceSnap!: FaceSnap;
-
-  constructor(private faceSnapsService: FaceSnapsService,
-              private router: Router) {
-    
-  }
+export class SingleFaceSnapComponent implements OnInit {
+  faceSnap!: FaceSnap;
+  buttonText!: FaceSnap;
+  constructor(private faceSnapsService: FaceSnapsService, 
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-
+    const snapId = +this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapsService.getFaceSnapById(snapId);
   }
 
+  
   onToggleSnap() {
     if(!this.faceSnap.userSnapped) {
       this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap')
@@ -31,9 +30,5 @@ export class FaceSnapComponent implements OnInit {
       this.faceSnap.btnContent = "Oh Snaps !";
     }
     this.faceSnap.userSnapped = !this.faceSnap.userSnapped;
-  }
-
-  onViewFaceSnap() {
-    this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`)
   }
 }
